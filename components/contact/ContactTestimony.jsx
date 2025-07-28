830076879ea610475e1141c9391026b9adaa8810
Rémi Tafforeau <remitafforeau@gmail.com>
2025-07-28 21:02:52 +0200

    Quotedblock composant

--- components/contact/ContactTestimony.jsx
+++ components/contact/ContactTestimony.jsx
@@ -1,19 +1,19 @@
 import Image from 'next/image';
+import QuotedBlock from '../ui-kit/QuotedBlock';
 
 export default function ContactTestimony() {
   return (
     <div className="space-y-4 prose">
       <h3 className="text-sm font-bold tracking-wide uppercase">ENEDIS</h3>
-      <blockquote className="italic">
-        <span className="text-4xl">“</span>
+      <QuotedBlock className="">
         Merci à Rémi pour son implication et son travail de qualité dans le développement de notre portail web. Son intervention a permis 
         l’installation fluide de notre nouveau service.
-      </blockquote>
+      </QuotedBlock>
 
       <div className="flex items-center gap-4 mt-4">
         <Image
           src="/assets/images/avatar-bruno.png"
-          alt="Pierre S."
+          alt="Bruno G."
           width={40}
           height={40}
           className="rounded-full object-cover"
